3d1f96293ac1b4084cbafd12a91500cf4d3c1166
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-08-06 07:37:43 -0400

    compared code to class notes

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ a fun web application for comparing and predicting tweet authorship
 This is the class notes and work for DSPT6 
 WebApp101 is based off of DS15
 
+https://github.com/bruno-janota/DSPT6-Twitoff
+
 TODO: base.html
--- twitoff/db_model.py
+++ twitoff/db_model.py
@@ -1,16 +1,17 @@
 '''SQLAlchemy models for Twitoff'''
-
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    follower_count = db.Column(db.Integer, unique=True, nullable=False)
+    follower_count = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
+
 
 class Tweet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -19,3 +20,4 @@ class Tweet(db.Model):
 
     def __repr__(self):
         return '<Tweet %r>' % self.text
+
